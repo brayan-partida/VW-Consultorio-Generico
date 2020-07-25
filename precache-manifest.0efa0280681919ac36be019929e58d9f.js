7eeb05c1cec52dae8c4b5acb4f74c136bdb85a88
brayan-partida <brenramirezpa@ittepic.edu.mx>
2020-07-25 15:38:40 -0600

    Updates

---
+++ precache-manifest.0efa0280681919ac36be019929e58d9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5a11e0e6fe8663520d5d3a10506360",
+    "url": "/VW-Consultorio-Generico/index.html"
+  },
+  {
+    "revision": "a8e3c6c34a6b85e880be",
+    "url": "/VW-Consultorio-Generico/static/css/main.3ec473bc.chunk.css"
+  },
+  {
+    "revision": "fdff1346e63b2a2f5ef7",
+    "url": "/VW-Consultorio-Generico/static/js/2.36cbdc2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VW-Consultorio-Generico/static/js/2.36cbdc2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e3c6c34a6b85e880be",
+    "url": "/VW-Consultorio-Generico/static/js/main.4f6dfe5b.chunk.js"
+  },
+  {
+    "revision": "7ac87ecaf0f534f83eba",
+    "url": "/VW-Consultorio-Generico/static/js/runtime-main.74c25a69.js"
+  }
+]);
